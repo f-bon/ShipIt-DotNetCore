957ddad869d6e31e1998e028689bd933a4e0fb01
Vani85 <vani.natraj@gmail.com>
2025-03-19 12:00:35 +0000

    removed key annotation

--- ShipIt/Models/DataModels/CompanyDataModel.cs
+++ ShipIt/Models/DataModels/CompanyDataModel.cs
@@ -6,7 +6,6 @@ namespace ShipIt.Models.DataModels
 {
     public class CompanyDataModel : DataModel
     {
-        [Key]
         [DatabaseColumnName("gcp_cd")]
         public string Gcp { get; set; }
         [DatabaseColumnName("gln_nm")]
